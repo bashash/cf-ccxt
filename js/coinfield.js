1e8fd5acb54382966ac37072cd590e926db462ab
Shingo Konnai <shingo@ShingoKonnaiMacBook.local>
2020-03-25 21:18:56 -0700

    fix the api base url for tapiPrivate cointiger

--- js/coinfield.js
+++ js/coinfield.js
@@ -71,4 +71,26 @@ module.exports = class coinfield extends Exchange {
             },
         });
     }
+    sign(path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+        let request = '/';
+        request += path;
+        if (api === 'public') {
+            if (method === 'GET') {
+                if (Object.keys(params).length) {
+                    const { limit, timestamp, from, order_by} = params;
+                    // request += '?' +
+                }
+            }
+        } else if (api === 'private') {
+            // const jwt = this.jwt (request, this.encode (this.secret));
+            this.apiKey = 'eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1ODUxNzg5ODEsImV4cCI6MTU4NTI2NTMzOCwic3ViIjoic2Vzc2lvbiIsImlzcyI6InVhYyIsInNjcCI6WyJvcmRlcnMiXSwiYXBpIjp0cnVlLCJqdGkiOiJiMzk1NTZiNy04MWE4LTRkMDAtYTRmMS04ZDgwODU2ZGQzZDgiLCJ1aWQiOiJJRDQwMEMyRDRBMzYiLCJlbWFpbCI6ImRlbW9AY29pbmZpZWxkLmNvbSJ9.Yp61vZSBEFjLl16Cr1MUSLWYvpaNAXnw6GkE5T2Ig720e5-c4h9ykCTGXIUuwFp2owfsnD2SeuPs68ngs_N9FOsRNHlWxol-0zlGKzAHKgH5k_FL_7XELi_K7vGjN5IzYH_FIr_G1ujgV55tWIq61tFoqAIUyBVZ3whqKrU6srCHYyapCyBPNeOFD_GVb18Q_Lr1J24LTAkmDdiabNTiCEZOfmpAvkULrXpo3gLtXWhEqvTX2kMNWBI91NB5VxXphFyCzoi2O28JznKWXbn7oTWusSMdxAMyU2weg09phTwGag5IeX_yvP1qr0F1e4MjENuzMXz97J1gIVCe8JXWlg'
+            headers = {
+                'Authorization': 'Bearer ' + this.apiKey,
+            }
+        }
+        const url = this.urls['api'] + request;
+
+        return { 'url': url, 'method': method, 'body': body, 'headers': headers };
+    }
+
 }
