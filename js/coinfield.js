98209b123d84be878991297c9014e1982e6fbb67
Shingo Konnai <shingo@ShingoKonnaiMacBook.local>
2020-03-26 15:39:59 -0700

    remove an api key in the code in coinfield

--- js/coinfield.js
+++ js/coinfield.js
@@ -444,7 +444,6 @@ module.exports = class coinfield extends Exchange {
         let request = '/';
         request += this.implodeParams (path, params);
         if (api === 'private') {
-            this.apiKey = 'eyJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1ODUxNzg5ODEsImV4cCI6MTU4NTI2NTMzOCwic3ViIjoic2Vzc2lvbiIsImlzcyI6InVhYyIsInNjcCI6WyJvcmRlcnMiXSwiYXBpIjp0cnVlLCJqdGkiOiJiMzk1NTZiNy04MWE4LTRkMDAtYTRmMS04ZDgwODU2ZGQzZDgiLCJ1aWQiOiJJRDQwMEMyRDRBMzYiLCJlbWFpbCI6ImRlbW9AY29pbmZpZWxkLmNvbSJ9.Yp61vZSBEFjLl16Cr1MUSLWYvpaNAXnw6GkE5T2Ig720e5-c4h9ykCTGXIUuwFp2owfsnD2SeuPs68ngs_N9FOsRNHlWxol-0zlGKzAHKgH5k_FL_7XELi_K7vGjN5IzYH_FIr_G1ujgV55tWIq61tFoqAIUyBVZ3whqKrU6srCHYyapCyBPNeOFD_GVb18Q_Lr1J24LTAkmDdiabNTiCEZOfmpAvkULrXpo3gLtXWhEqvTX2kMNWBI91NB5VxXphFyCzoi2O28JznKWXbn7oTWusSMdxAMyU2weg09phTwGag5IeX_yvP1qr0F1e4MjENuzMXz97J1gIVCe8JXWlg'
             if (method === 'POST') {
                 headers = {
                     'Content-Type': 'application/json',
@@ -460,7 +459,6 @@ module.exports = class coinfield extends Exchange {
             }
         }
         const url = this.urls['api'] + request;
-        console.log("HEREEEE", url)
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
 
