01fbe34944d15fb336867aaaed9b190f1f049951
Shingo Konnai <shingo@ShingoKonnaiMacBook.local>
2020-04-14 12:47:14 -0700

    fix the url of create order in tokensnet

--- js/tokensnet.js
+++ js/tokensnet.js
@@ -55,7 +55,7 @@ module.exports = class tokensnet extends Exchange {
                         'private/trades/{tradingPair}/{page}/',
                     ],
                     'post': [
-                        'private/orders/add/limit',
+                        'private/orders/add/limit/',
                         'private/orders/cancel/{orderId}/'
                     ],
                 },
