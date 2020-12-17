b94bf3d0569c0aea9105b922212a082c827fd1b2
Shingo Konnai <shingo@ShingoKonnaiMacBook.local>
2020-12-16 16:27:31 -0800

    Bilaxy update fetch orders, fetch open orders, fetch my trades, fetch balance to new api and fix old endpoint

--- js/bilaxy.js
+++ js/bilaxy.js
@@ -36,7 +36,8 @@ module.exports = class bilaxy extends Exchange {
                 'logo': 'https://avatars2.githubusercontent.com/u/58157269?s=400&u=0c535058316c0f03287519950ef5f2d61ce27093&v=4',
                 'api': {
                     "public": 'https://newapi.bilaxy.com/v1',
-                    "private": 'https://api.bilaxy.com/v1',//'https://newapi.bilaxy.com/v1'
+                    "oldprivate": 'https://api.bilaxy.com/v1',
+                    "private": 'https://newapi.bilaxy.com/v1',
                 },
                 'www': 'https://bilaxy.com/',
                 'doc': 'https://github.com/bilaxy-exchange/bilaxy-api-docs',
@@ -52,15 +53,27 @@ module.exports = class bilaxy extends Exchange {
                 },
                 'private': {
                     'get': [
-                        'balances',//accounts/balances
-                        'trade_list',//open orders => accounts/orders/opened, all orders => accounts/orders
-                        'trade_view',//accounts/trades
+                        'accounts/balances',
+                        'accounts/orders',
+                        'accounts/orders/opened',
+                        'accounts/trades'
+                    ],
+                    // 'post': [
+                    //     'trade',
+                    //     'cancel_trade'
+                    // ],
+                },
+                'oldprivate': {
+                    'get': [
+                        'balances',//old api endpoint. deprecated
+                        'trade_list',//old api endpoint. deprecated
+                        'trade_view',//old api endpoint. deprecated
                     ],
                     'post': [
                         'trade',
                         'cancel_trade'
                     ],
-                },
+                }
             },
         });
     }
@@ -105,17 +118,34 @@ module.exports = class bilaxy extends Exchange {
 
     async fetchBalance () {
         await this.loadMarkets();
-        const response = await this.privateGetBalances();
-        const balances = this.safeValue (response, 'data');
+        // const response = await this.privateGetBalances();
+        // {
+        //     "BTC": {
+        //       "available": 0.3851212,
+        //       "used": 0
+        //     },
+        //     "ETH": {
+        //       "available": 0.12,
+        //       "used": 0.346
+        //     },
+        //     "USDT": {
+        //       "available": 39.289,
+        //       "used": 0
+        //     }
+        //   }
+        const response = await this.privateGetAccountsBalances();
+        // console.log("fetchBalance", response)
+        // const balances = this.safeValue (response, 'data');
+        const balances = Object.entries(response);
         const result = { 'info': response };
         for (let i = 0; i < balances.length; i++) {
-            const balance = balances[i];
-            const currencyId = this.safeString (balance, 'name');
-            const code = this.safeCurrencyCode (currencyId);
+            const currency = balances[i][0];
+            const balance = balances[i][1];
+            const code = this.safeCurrencyCode (currency);
             const account = this.account ();
-            account['free'] = this.safeFloat (balance, 'balance');
-            account['used'] = this.safeFloat (balance, 'frozen');
-            const total = this.safeFloat (balance, 'balance') + this.safeFloat (balance, 'frozen');
+            account['free'] = this.safeFloat (balance, 'available');
+            account['used'] = this.safeFloat (balance, 'used');
+            const total = this.safeFloat (balance, 'available') + this.safeFloat (balance, 'used');
             account['total'] = total;
             result[code] = account;
         }
@@ -242,13 +272,23 @@ module.exports = class bilaxy extends Exchange {
         await this.loadMarkets ();
         const market = this.market(symbol);
         const id = this.marketId(symbol);
+        // const request = {
+        //     'symbol': id,
+        //     'since': since ? since : 0,
+        //     // 'type': 0,
+        // }
+        // const response = await this.privateGetTradeList(this.extend(request, params));
+        // new endpoint 'accounts/orders'
+        // console.log('market', market);
+        // const timestamp = new Date().getTime();
+        const pair = market.base + '_' + market.quote;
         const request = {
-            'symbol': id,
-            'since': since ? since : 0,
-            // 'type': 0,
-        }
-        const response = await this.privateGetTradeList(this.extend(request, params));
-        return this.parseOrders (response.data, market, since, limit);
+            pair: pair,
+            limit: limit,
+        };
+        // console.log('request', request)
+        const response = await this.privateGetAccountsOrders(this.extend(request, params));
+        return this.parseOrders (response, market, since, limit);
     }
 
     async fetchOpenOrders (symbol = undefined, since = undefined, limit = 100, params = {}) {
@@ -258,13 +298,28 @@ module.exports = class bilaxy extends Exchange {
         await this.loadMarkets ();
         const market = this.market(symbol);
         const id = this.marketId(symbol);
+        const pair = market.base + '_' + market.quote;
         const request = {
-            'symbol': id,
-            'since': since ? since : 0,
+            'pair': pair,
+            // 'since': since ? since : 0,
             // 'type': 1,
         }
-        const response = await this.privateGetTradeList(this.extend(request, params));
-        return this.parseOrders (response.data, market, since, limit);
+        // const response = await this.privateGetTradeList(this.extend(request, params));
+        // accounts/orders/opened
+        const response = await this.privateGetAccountsOrdersOpened(this.extend(request, params));
+        // [ {
+        //     "id": 4141252871,
+        //     "price": 6998,
+        //     "amount": 0.71327,
+        //     "total": 4991.46346,
+        //     "filled_total": 0,
+        //     "created_at": "2020-04-06T13:57:03Z",
+        //     "pair_name": "BTC_USDT",
+        //     "direction": "sell",
+        //     "filled_amount": 0,
+        //     "state": "non-filled"
+        //   }, .. ]
+        return this.parseOrders (response, market, since, limit);
     }
 
     parseOrders (orders, market = undefined, since = undefined, limit = undefined, params = {}) {
@@ -284,16 +339,30 @@ module.exports = class bilaxy extends Exchange {
     }
 
     parseOrder (order, market = undefined) {
+        // {
+        //     "id": 4141252868,
+        //     "price": 7012.5,
+        //     "amount": 0.008939,
+        //     "total": 62.6847375,
+        //     "filled_total": 0,
+        //     "created_at": "2020-04-06T13:56:55Z",
+        //     "pair_name": "BTC_USDT",
+        //     "direction": "sell",
+        //     "filled_amount": 0,
+        //     "state": "canceled"
+        //   },
         const { id } = order;
-        const timestamp = new Date(this.safeString (order, 'datetime')).getTime();
+        const timestamp = new Date(this.safeString (order, 'created_at')).getTime();
         const datetime = this.iso8601(timestamp);
         const symbol = market;
-        const status = this.parseOrderStatus(order.status);
+        // const status = this.parseOrderStatus(order.state);
+        const status = this.safeString(order, 'state');
         const price = this.safeFloat(order, 'price');
-        const side = this.safeString(order, 'type');
-        const amount = this.safeFloat(order, 'count');
-        const cost = Number(order.price) * Number(order.amount);
-        const remaining = this.safeFloat(order, 'left_count');
+        const side = this.safeString(order, 'direction');
+        const amount = this.safeFloat(order, 'amount');
+        const cost = this.safeFloat(order, 'total');
+        const filled = this.safeFloat(order, 'filled_total');
+        const remaining = Number(amount) - Number(filled);
 
         return {
             'id': id,
@@ -308,62 +377,80 @@ module.exports = class bilaxy extends Exchange {
             'average': undefined,
             'cost': cost,
             'amount': amount,
-            'filled': undefined,
+            'filled': filled,
             'remaining': remaining,
             'fee': undefined,
             'info': order,
         }
     }
 
-    // async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-    //     if (symbol === undefined) {
-    //         throw new ArgumentsRequired (this.id + ' fetchMyTrades requires a `symbol` argument');
-    //     }
-    //     await this.loadMarkets ();
-    //     const market = this.market (symbol);
-    //     const request = {
-    //         'market': market['id'],
-    //         'limit': limit ? limit : 50,
-    //     };
+    async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        if (symbol === undefined) {
+            throw new ArgumentsRequired (this.id + ' fetchMyTrades requires a `symbol` argument');
+        }
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        // console.log("market", market)
+        const pair = market.base + '_' + market.quote;
+        const request = {
+            'pair': pair,
+            'limit': limit ? limit : 100,
+            'end': since ? since : new Date().getTime() - 24 * 60 * 60 * 1000 * 10,//10 days before now
+        };
 
-    //     const response = await this.privateGetTradeView (this.extend (request, params));
-    //     return this.parseMyTrades (response.trades, symbol, since, limit);
-    // }
+        // const response = await this.privateGetTradeView (this.extend (request, params));
+        const response = await this.privateGetAccountsTrades (this.extend (request, params));
+        return this.parseMyTrades (response, symbol, since, limit);
+    }
 
-    // parseMyTrades (trades, market = undefined, since = undefined, limit = undefined, params = {}) {
-    //     let result = Object.values (trades || []).map ((trade) => this.extend (this.parseMyTrade (trade, market), params))
-    //     result = this.sortBy (result, 'timestamp')
-    //     return result;
-    // }
+    parseMyTrades (trades, market = undefined, since = undefined, limit = undefined, params = {}) {
+        let result = Object.values (trades || []).map ((trade) => this.extend (this.parseMyTrade (trade, market), params))
+        result = this.sortBy (result, 'timestamp')
+        return result;
+    }
 
-    // parseMyTrade (trade, market = undefined) {
-    //     const {
-    //         id,
-    //         side,
-    //     } = trade;
-    //     const timestamp = this.parse8601 (this.safeString (trade, 'timestamp'));
-    //     const datetime = this.iso8601(timestamp);
-    //     const symbol = market;
-    //     const orderId = this.safeString(trade, 'order_id');
-    //     const price = this.safeFloat(trade, 'price');
-    //     const amount = this.safeFloat(trade, 'volume');
-    //     const cost = this.safeFloat(trade, 'total_value');
-    //     return {
-    //         'id': id,
-    //         'timestamp': timestamp,
-    //         'datetime': datetime,
-    //         'symbol': symbol,
-    //         'order': orderId,
-    //         'type': undefined,
-    //         'side': side,
-    //         'takerOrMaker': undefined,
-    //         'price': price,
-    //         'amount': amount,
-    //         'cost': cost,
-    //         'fee': undefined,
-    //         'info': trade,
-    //     };
-    // }
+    parseMyTrade (trade, market = undefined) {
+        // {
+        //     "id": 716797287,
+        //     "order_id": 4141266876,
+        //     "total": 0.0345,
+        //     "price": 0.0000345,
+        //     "amount": 1000,
+        //     "created_at": "2020-04-10T19:00:14Z",
+        //     "role": "taker",
+        //     "direction": "buy",
+        //     "pair": "BIA_ETH",
+        //     "fees": 0.96189591,
+        //     "fees_currency": "BIA"
+        //   },
+        const id = this.safeString(trade, 'id');
+        const timestamp = this.parse8601 (this.safeString (trade, 'created_at'));
+        const datetime = this.iso8601(timestamp);
+        const symbol = market;
+        const orderId = this.safeString(trade, 'id');
+        const price = this.safeFloat(trade, 'price');
+        const amount = this.safeFloat(trade, 'amount');
+        const cost = this.safeFloat(trade, 'total');
+        const fee = this.safeFloat(trade, 'fees');
+        const side = this.sageString(trade, 'direction');
+        const takeOrMaker = this.safeString(trade, 'role');
+
+        return {
+            'id': id,
+            'timestamp': timestamp,
+            'datetime': datetime,
+            'symbol': symbol,
+            'order': orderId,
+            'type': undefined,
+            'side': side,
+            'takerOrMaker': takeOrMaker,
+            'price': price,
+            'amount': amount,
+            'cost': cost,
+            'fee': fee,
+            'info': trade,
+        };
+    }
 
     async createOrder (symbol, type = 'limit', side, amount, price = undefined, params = {}) {
         await this.loadMarkets ();
@@ -375,7 +462,7 @@ module.exports = class bilaxy extends Exchange {
             'type': side,
         }
 
-        const response = await this.privatePostTrade(this.extend(request, params));
+        const response = await this.oldprivatePostTrade(this.extend(request, params));
         const { data } = response;
         return {
             'id': data,
@@ -391,7 +478,7 @@ module.exports = class bilaxy extends Exchange {
         const request = {
             'id': id,
         };
-        return await this.privatePostCancelTrade (this.extend (request, params));
+        return await this.oldprivatePostCancelTrade (this.extend (request, params));
     }
 
     sha1 (request, secret, hash = 'sha256', digest = 'hex') {
@@ -403,7 +490,7 @@ module.exports = class bilaxy extends Exchange {
         return result
     }
 
-    createSignature (input) {
+    createSignatureOldApi (input) {
         const params = [];
         for (let i in input) {
             params.push(`${i}=` + input[i]);   
@@ -413,11 +500,27 @@ module.exports = class bilaxy extends Exchange {
         return signature;
     }
 
+    createSignature (params) {
+        const query = [];        
+        for (let i in params) {
+            query.push(`${i}=` + params[i]);
+        };
+        const queryString = query.sort().join('&');
+        const signature = this.hmac (this.encode (queryString), this.encode (this.secret), 'sha256', 'hex');
+        return signature;
+    }
+
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let request = '/';
         request += path;
 
         if (api === 'private') {
+            // const signature = this.createSignature({ ...params, key: this.apiKey, secret: this.secret });
+            // request += '?' + this.urlencode ({ ...params, key: this.apiKey, sign: signature });
+            const timestamp = new Date().getTime();
+            const signature = this.createSignature({ ...params, apikey: this.apiKey, timestamp: timestamp });
+            request += '?' + this.urlencode ({ ...params, apikey: this.apiKey, signature: signature, timestamp: timestamp });
+        } else if (api === 'oldprivate') {
             const signature = this.createSignature({ ...params, key: this.apiKey, secret: this.secret });
             request += '?' + this.urlencode ({ ...params, key: this.apiKey, sign: signature });
         } else {
